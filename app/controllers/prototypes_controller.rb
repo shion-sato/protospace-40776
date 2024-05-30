f93b7cc1e3dfa905b30bb719553a98c30c32f85f
shion-sato <rstoyo3.sato.shion@techcamp-training.com>
2024-05-30 13:02:32 +0900

    initial commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,55 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index, :create]
+
+  def  index
+    @prototype = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+    if @prototype.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless @prototype.user.id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
+    if @prototype.update(prototype_params)
+      redirect_to action: :show
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image, :user).merge(user_id: current_user.id)
+  end
+end
